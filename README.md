78f303b9beeffbf1f876bedbf8431928681ad218
Barry Moore <moore0557@gmail.com>
2019-11-21 22:30:40 -0500

    Move to Browser.Application, stack ingredient inputs, add subtraction buttons

--- README.md
+++ README.md
@@ -16,3 +16,11 @@ elm reactor
 - Open `localhost:8000` in your browser
 - Click `src`
 - Click `Main.elm`
+
+## To Do
+
+- Re-architect Main.elm to deal with multiple apps
+  on a single page (requires reading!)
+- `Recipes.Add`, when exists, would benefit from
+  - Auto-add item if box is not empty
+  - Auto-collapse empty boxes at end
--- src/Main.elm
+++ src/Main.elm
@@ -1,76 +1,129 @@
 module Main exposing (..)
 
--- Input a user name and password. Make sure the password matches.
---
--- Read how it works:
---   https://guide.elm-lang.org/architecture/forms.html
---
-
 import Browser
+import Browser.Navigation as Nav
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput, onClick)
+import Url
 import Array
 
 -- MAIN
 
+main : Program () Model Msg
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+    Browser.application
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        , onUrlChange = UrlChanged
+        , onUrlRequest = LinkClicked
+        }
+
 
 -- MODEL
+
 
 type alias Model =
   { ingredients : Array.Array (Int, String)
   , count : Int
+  , key : Nav.Key
+  , url : Url.Url
   }
 
-init : Model
-init =
-  Model (Array.fromList [(0, "")]) 1
+init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
+init flags url key =
+  (Model (Array.fromList [(0, "")]) 1 key url, Cmd.none)
 
 -- UPDATE
 
 type Msg
   = Add
   | Set Int String
+  | Remove Int
+  | LinkClicked Browser.UrlRequest
+  | UrlChanged Url.Url
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
     Add ->
-      { model
+      ( { model
         | ingredients = Array.append
           model.ingredients (Array.fromList [(model.count, "")])
         , count = model.count + 1
-      }
+        }
+      , Cmd.none
+      )
     Set idx v ->
-      { model
-        | ingredients =  model.ingredients
+      ( { model
+          | ingredients =  model.ingredients
                       |> Array.set idx (idx, v)
-      }
+        }
+      , Cmd.none
+      )
+    Remove idx ->
+      let firstNotEqIdx = Tuple.first >> (==) idx >> not
+          ingredientStrs = Array.filter firstNotEqIdx model.ingredients
+            |> Array.map Tuple.second
+      in
+          ( { model
+              | count = model.count - 1
+              , ingredients = Array.toIndexedList ingredientStrs |> Array.fromList
+            }
+          , Cmd.none
+          )
+    LinkClicked urlRequest ->
+      case urlRequest of
+         Browser.Internal url ->
+           ( model, Nav.pushUrl model.key (Url.toString url) )
+
+         Browser.External href ->
+           ( model, Nav.load href )
+
+    UrlChanged url ->
+      ( { model | url = url }
+      , Cmd.none
+      )
 
 -- VIEW
 
-view : Model -> Html Msg
+view : Model -> Browser.Document Msg
 view model =
-  List.concat
-  [ Array.map (\(idx, v) -> viewInput "text" "Ingredient" idx v)
-      model.ingredients
-      |> Array.toList
-  , List.singleton ( button [ onClick Add ] [ text "+" ] )
-  , List.singleton (viewValidation model)
-  ] |>
-  div []
+  { title = "Just Recipes"
+  , body =
+    [
+      div [] [
+        div []
+          ( Array.map (\(idx, v) -> viewInput "text" "Ingredient" idx v)
+            model.ingredients
+            |> Array.toList
+          )
+        , div [] [ button [ onClick Add ] [ text "+" ] ]
+        , div [] [ viewValidation model ]
+        ]
+    ]
+  }
 
 viewInput : String -> String -> Int -> String -> Html Msg
 viewInput t p idx v =
-  input [ type_ t, placeholder p, value v, onInput (Set idx) ] []
+  div []
+    [ input [ type_ t, placeholder p, value v, onInput (Set idx) ] []
+    , button [ onClick (Remove idx) ] [ text "-" ]
+    ]
 
 viewValidation : Model -> Html msg
 viewValidation model =
-  if Array.toList model.ingredients
-     |> List.all (Tuple.second >> String.isEmpty >> not)
-  then
+  if model.count == 0 then
+    div [ style "color" "red" ] [ text "Recipe has no ingredients?" ]
+  else if Array.toList model.ingredients
+     |> List.all (Tuple.second >> String.isEmpty >> not) then
     div [ style "color" "green" ] [ text "Nice recipe!"]
   else
-    div [ style "color" "red" ] [ text "Empty ingredients?" ]
+    div [ style "color" "red" ] [ text "Empty ingredients box?" ]
+
+-- SUBSCRIPTIONS
+
+subscriptions : Model -> Sub Msg
+subscriptions _ = Sub.none
